c6259c02a0ae6467abaf7a54a37d230329836aad
Ibrahim-Fateen <80049894+Ibrahim-Fateen@users.noreply.github.com>
2024-10-10 20:40:18 +0300

    Initial Commit

---
+++ OpenGL-setup/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OpenGL-setup
+//
+//  Created by Ibrahim Fateen on 10/10/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
